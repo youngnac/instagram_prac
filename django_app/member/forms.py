e8819fb2cd99de3818798f87481d763c38182168
youngnac <youngnac@gmail.com>
2017-02-14 15:41:50 +0900

    login view created

---
+++ django_app/member/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=20)
+    password = forms.CharField(widget=forms.PasswordInput())
+
--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import LoginForm
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/admin')
+            else:
+                form.add_error(None,'ID or PW incorrect')
+                # return HttpResponse('Login Failed')
+    else:
+        form = LoginForm()
+
+    context = {
+        'form': form,
+        }
+    return render(request, 'member/login.html', context)
