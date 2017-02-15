84582bdba1b943f0835815236cbaef7903f60930
youngnac <youngnac@gmail.com>
2017-02-15 20:19:06 +0900

    create_user, create_superuser, MyUser,MyUserManager edited, done for now

--- django_app/member/admin.py
+++ django_app/member/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
+
 from .models import MyUser
+
 
 # Register your models here.
 
-admin.site.register(MyUser)
+class MyUserAdmin(UserAdmin):
+    list_display = ['username', 'nickname']
+    list_filter = ['is_staff', 'is_superuser', ]
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('gender', 'nickname', 'email')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'email'),
+        }),
+    )
+
+
+admin.site.register(MyUser, MyUserAdmin)
--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,5 +1,46 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
 
 
-class MyUser(AbstractBaseUser):
-    pass
+class MyUserManager(BaseUserManager):
+    def create_user(self, username, password=None):
+        user = self.model(username=username)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, password):
+        user = self.model(username=username)
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class MyUser(PermissionsMixin, AbstractBaseUser):
+    CHOICES_GENDER = (
+        ('m', 'male'),
+        ('f', 'female'),
+
+    )
+    # since inheriting from AbstractBaseUser
+    # password
+    # last_loginmg
+    # is_active are given
+
+    username = models.CharField(max_length=25, unique=True)
+    nickname = models.CharField(max_length=20)
+    email = models.EmailField(blank=True)
+    gender = models.CharField(max_length=1, choices=CHOICES_GENDER)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    objects = MyUserManager()
+
+    def get_short_name(self):
+        return "Hello, {}! Your nickname is {}".format(self.username, self.nickname)
+    def get_full_name(self):
+        return self.nickname
+
